dd1a141de8e176f132f6c82822c9c245e1ff45fd
pwgith <131629357+pwgith@users.noreply.github.com>
2023-06-28 08:05:00 +0000

    interim

--- o2k.py
+++ o2k.py
@@ -1,43 +1,8 @@
-from swagger_parser import SwaggerParser
+# from openapi_parser import parse
+import openapi_parser
 
-def generate_karate_test_cases(swagger_file):
-    parser = SwaggerParser(swagger_file)
-    paths = parser.paths
+specification = parse('swagger.yml')
 
-    test_cases = []
+urls = [x.url for x in specification.paths]
 
-    for path, path_item in paths.items():
-        for method, operation in path_item.items():
-            test_case = f"""
-Scenario: Test {operation['operationId']}
-    Given url {parser.host}{parser.base_path}{path}
-    When method {method.upper()}
-"""
-
-            # Process request parameters
-            parameters = operation.get('parameters', [])
-            optional_parameters = [param for param in parameters if param.get('required') is False]
-            mandatory_parameters = [param for param in parameters if param.get('required') is True]
-
-            if optional_parameters:
-                test_case += "    And request {} = <optional_value>\n".format(
-                    ', '.join([param['name'] for param in optional_parameters])
-                )
-
-            if mandatory_parameters:
-                test_case += "    And request {} = <mandatory_value>\n".format(
-                    ', '.join([param['name'] for param in mandatory_parameters])
-                )
-
-            test_case += "    Then status {operation['responses'].keys()[0]}\n"
-
-            test_cases.append(test_case)
-
-    return '\n'.join(test_cases)
-
-
-# Example usage
-swagger_file = 'petstore_example/swagger.json'
-karate_test_cases = generate_karate_test_cases(swagger_file)
-
-print(karate_test_cases)
+print(urls)
